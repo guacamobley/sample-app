93c69c0ffca2dfdd3c3d12624448c6bb7cc5c1bb
Gary Mobley <gary.mobley.jr@gmail.com>
2020-02-13 10:07:20 -1000

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -21,7 +21,10 @@ module SessionsHelper
         @current_user = user
       end
     end
+  end
 
+  def current_user?(user)
+    user == current_user
   end
 
   def logged_in?
@@ -38,6 +41,17 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  #redirects to stored location (or to the default)
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  #stores the URL trying to be accessed
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,6 +5,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
   # 
+  # 
+  def setup
+    @user = users(:michael)
+  end
   
   test "layout links" do
     get root_path
@@ -13,6 +17,16 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", edit_user_path(@user), count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+    assert_select "a[href=?]", logout_path, count: 0
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", login_path, count: 0
     get contact_path
     assert_select "title", full_title("Contact")
     get signup_path
